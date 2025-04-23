509f69194b51c6ea0e002ef67daac51dbaba00db
19013na <19013@naver.com>
2025-04-23 10:52:24 +0900

    init : java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * 자동완성 단축키 : ctrl + space
+ * javac HelloWorld.java
+ * java HelloWorld
+ * Run 단축키 : ctrl + f11 
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello, java~");
+	}
+}
